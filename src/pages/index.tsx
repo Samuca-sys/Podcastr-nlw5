da7dd2791bc4e01cae9da4423bb666a9fddee6e1
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-21 22:15:57 -0300

    ci: add Episode Dynammic Static Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -12,7 +13,6 @@ type Episode = {
 	id: string;
 	title: string;
 	thumbnail: string;
-	description: string;
 	members: string;
 	duration: number;
 	durationAsString: string;
@@ -44,7 +44,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 								/>
 
 								<div className={styles.episodeDetails}>
-									<a href=''>{episode.title}</a>
+									<Link href={`/episodes/${episode.id}`}>
+										<a>{episode.title}</a>
+									</Link>
 									<p>{episode.members}</p>
 									<span>{episode.publishedAt}</span>
 									<span>{episode.durationAsString}</span>
@@ -64,12 +66,14 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
 				<table cellSpacing={0}>
 					<thead>
-						<th></th>
-						<th>Podcast</th>
-						<th>Integrantes</th>
-						<th>Data</th>
-						<th>Duração</th>
-						<th></th>
+						<tr>
+							<th></th>
+							<th>Podcast</th>
+							<th>Integrantes</th>
+							<th>Data</th>
+							<th>Duração</th>
+							<th></th>
+						</tr>
 					</thead>
 
 					<tbody>
@@ -86,7 +90,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 										/>
 									</td>
 									<td>
-										<a href=''>{episode.title}</a>
+										<Link href={`/episodes/${episode.id}`}>
+											<a>{episode.title}</a>
+										</Link>
 									</td>
 									<td>{episode.members}</td>
 									<td style={{ width: 100 }}>{episode.publishedAt}</td>
@@ -128,7 +134,6 @@ export const getStaticProps: GetStaticProps = async () => {
 			durationAsString: convertDurationToTimeString(
 				Number(episode.file.duration)
 			),
-			description: episode.description,
 			url: episode.file.url,
 		};
 	});
