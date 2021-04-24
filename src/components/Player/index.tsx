8c9bf673d62420f422da81c52a170af7e5e76228
Samuel Macedo <samuel.macfer@gmail.com>
2021-04-23 21:57:38 -0300

    ci: add audio, play and pause using mouse or keyboard through React ref

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,17 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import Image from 'next/image';
 
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css'; //default styles from rc-slider package
+import 'rc-slider/assets/index.css';
 
 import { PlayerContext } from '../../contexts/PlayerContext';
 
 import styles from './styles.module.scss';
 
 export function Player() {
-	const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+	const audioRef = useRef<HTMLAudioElement>(null);
+	const {
+		episodeList,
+		currentEpisodeIndex,
+		isPlaying,
+		togglePlay,
+		setPlayingState,
+	} = useContext(PlayerContext);
 
 	const episode = episodeList[currentEpisodeIndex];
+
+	useEffect(() => {
+		if (!audioRef.current) {
+			return;
+		}
+		if (isPlaying) {
+			audioRef.current.play();
+		} else {
+			audioRef.current.pause();
+		}
+	}, [isPlaying]);
+
 	return (
 		<div className={styles.playerContainer}>
 			<header>
@@ -19,7 +38,6 @@ export function Player() {
 				<strong>Tocando agora</strong>
 			</header>
 
-			{/* if episode not null */}
 			{episode ? (
 				<div className={styles.currentEpisode}>
 					<Image
@@ -56,6 +74,16 @@ export function Player() {
 					<span>00:00</span>
 				</div>
 
+				{episode && (
+					<audio
+						src={episode.url}
+						ref={audioRef}
+						autoPlay
+						onPlay={() => setPlayingState(true)}
+						onPause={() => setPlayingState(false)}
+					/>
+				)}
+
 				<div className={styles.buttons}>
 					<button type='button' disabled={!episode}>
 						<img src='/shuffle.svg' alt='Embaralhar' />
@@ -67,8 +95,13 @@ export function Player() {
 						type='button'
 						className={styles.playButton}
 						disabled={!episode}
+						onClick={togglePlay}
 					>
-						<img src='/play.svg' alt='Tocar' />
+						{isPlaying ? (
+							<img src='/pause.svg' alt='Tocar' />
+						) : (
+							<img src='/play.svg' alt='Parar' />
+						)}
 					</button>
 					<button type='button' disabled={!episode}>
 						<img src='/play-next.svg' alt='Tocar próximo' />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/global.scss';
+import { useState } from 'react';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
@@ -6,19 +6,38 @@ import { Player } from '../components/Player';
 import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
-import { useState } from 'react';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
 	const [episodeList, setEpisodeList] = useState([]);
 	const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false);
 
 	function play(episode) {
 		setEpisodeList([episode]);
 		setCurrentEpisodeIndex(0);
+		setIsPlaying(true);
+	}
+
+	function togglePlay() {
+		setIsPlaying(!isPlaying);
+	}
+
+	function setPlayingState(state: boolean) {
+		setIsPlaying(state);
 	}
 
 	return (
-		<PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+		<PlayerContext.Provider
+			value={{
+				episodeList,
+				currentEpisodeIndex,
+				play,
+				togglePlay,
+				isPlaying,
+				setPlayingState,
+			}}
+		>
 			<div className={styles.appWrapper}>
 				<main>
 					<Header />
